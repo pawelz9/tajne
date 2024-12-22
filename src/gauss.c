c9a6f074a808f225f5e0b1356361839e0962fa60
pawelz9 <pawelzawisl@gmail.com>
2024-12-22 18:45:40 +0100

    improve gauss.c

--- src/gauss.c
+++ src/gauss.c
@@ -1,5 +1,5 @@
 #include "gauss.h"
-
+#include <stdio.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
@@ -10,19 +10,20 @@ int eliminate(Matrix *mat, Matrix *b){
 		 */
 	double m=0;
 	
-	for(int i=0;i<mat->r-1;i++)
+	for(int i=0;i<mat->c-1;i++)
 	{
 		if(mat->data[i][i]==0)
 		{
 			printf("element diagonalny rowny zero nie udalo sie dokonac eliminacji gaussa");
 			return 1;
 		}
-			m=mat->data[i+1][i]/mat->data[i][i];
-			for(int j=0;j<mat->c;j++)
-			{
-				mat->data[i+1][j]-=mat->data[i][j]*m;
-			}
-			b->data[i+1][0]-=b->data[i][0]*m;
+		for(int j=i+1;j<mat->r;j++){
+				m=mat->data[j][i]/mat->data[i][i];
+				for(int k=i;k<mat->c;k++){
+					mat->data[j][k]-=mat->data[i][k]*m;
+				}
+				b->data[j][0]-=b->data[i][0]*m;
+		}	
 	}
 		return 0;
 }
