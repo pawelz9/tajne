7d8cc5f50dc99dcdffc16bdc24d817341745e96e
Delfinek777 <01197814@pw.edu.pl>
2024-12-26 18:50:01 +0100

    Zmieniłem backsubst, możesz sprawdzić czy wszystko gra

--- src/backsubst.c
+++ src/backsubst.c
@@ -4,20 +4,27 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+    if (mat->r != mat->c || mat->r != b->r || b->c != 1 || x->r != mat->r || x->c != 1) {
+        //błąd nieprawidłowych rozmiarów macierzy
+		printf("błąd nieprawidłowych rozmiarów macierzy");
+        return 2;
+    }
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-				int liczba = 10;
-				printf("pisanie kodu...\n");
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+    for (int i = mat->r - 1; i >= 0; i--) {
+        if (mat->data[i][i] == 0.0) {
+            //błąd dzielenia przez 0 (element na diagonali = 0)
+			printf("błąd dzielenia przez 0 (element na diagonali = 0)\n");
+            return 1;
+        }
 
-				return 0;
+        // Obliczanie wartosci x[i]
+        double sum = 0.0;
+        for (int j = i + 1; j < mat->c; j++) {
+            sum += mat->data[i][j] * x->data[j][0];
+        }
+        x->data[i][0] = (b->data[i][0] - sum) / mat->data[i][i];
+    }
+	
+    return 0; // wsteczne podstawienie zakonczone sukcesem
 }
-
-
